364ee7784f93cf18e954e52b276a09aff19e3187
Wade Stewart <stew.wade@gmail.com>
2018-05-22 12:57:06 -0400

    Hello World component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Hello from './Hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Hello />, 
+    document.getElementById('root'));
+    registerServiceWorker();
